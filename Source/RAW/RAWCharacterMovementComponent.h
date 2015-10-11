ed97cdecc83da721f9109efe7fe37568a46eecae
Clayton Langford <amplify91@gmail.com>
2015-10-11 17:13:42 -0400

    Version control artifacts from adding and deleting C++ stuff.
    Revert this if it breaks anything.

---
+++ Source/RAW/RAWCharacterMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "RAWCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAW_API URAWCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
